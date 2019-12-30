1fa7f97b54b9ff4e3e58f8c0e71b02a5cf74f798
Sarath Ravichandran <Sarath.Ravichandran@emishealth.com>
2019-12-30 16:42:09 +0530

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {Homepage} from './Components/Homepage/homepage.component'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dwarakesh</h1>
+      <Homepage />
     </div>
   );
 }
---
+++ src/Components/Homepage/homepage.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const Homepage = () => (
+    <div classname='homepage'>
+        <div classname='directory-menu'>
+          <div classname='directory-menu'>
+            <div classname ='menu-item'>
+              <div classname ='content'>
+                <h1 classname='title'>HATS</h1>
+                <span classname='subtitle'>SHOP NOW</span>
+              </div>    
+            </div>  
+            <div classname ='menu-item'>
+              <div classname ='content'>
+                <h1 classname='title'>JACKETS</h1>
+                <span classname='subtitle'>SHOP NOW</span>
+              </div>    
+            </div>   
+            <div classname ='menu-item'>
+              <div classname ='content'>
+                <h1 classname='title'>MENS</h1>
+                <span classname='subtitle'>SHOP NOW</span>
+              </div>    
+            </div>   
+            <div classname ='menu-item'>
+              <div classname ='content'>
+                <h1 classname='title'>WOMENS</h1>
+                <span classname='subtitle'>SHOP NOW</span>
+              </div>    
+            </div>    
+          </div>
+        </div>
+    </div>
+)
